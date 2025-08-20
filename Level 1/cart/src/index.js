53c63daa9fc22280bb44d1b62d98eddd7b3f0f45
Suman Manna <sumanmannacs1996@gmail.com>
2025-08-20 20:38:35 +0530

    Shared Module

--- Level 1/cart/src/index.js
+++ Level 1/cart/src/index.js
@@ -1,6 +1,1 @@
-import facker from "faker";
-
-const cartText = `<div>You have ${facker.random.number()} items in yout cart.</div>`;
-
-const root = document.getElementById("dev-cart");
-root.innerHTML = cartText;
+import("./bootstrap");
--- Level 1/products/src/index.js
+++ Level 1/products/src/index.js
@@ -1,13 +1,1 @@
-import facker from "faker";
-
-let products = "";
-
-for (let i = 0; i < 5; i++) {
-  const productName = facker.commerce.productName();
-  products += `<div>${productName}</div>`;
-}
-
-// console.log(products);
-
-const root = document.getElementById("dev-products");
-root.innerHTML = products;
+import("./bootstarp");
